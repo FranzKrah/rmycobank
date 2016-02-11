ac4984980710e1f5a9117dcd00573ea0ae134516
FranzKrah <f.krah@mailbox.org>
2016-02-11 19:24:58 +0100

    improoved synonym search

--- R/syno_mycobank.R
+++ R/syno_mycobank.R
@@ -13,27 +13,28 @@
 #' @author Franz-Sebastian Krah
 #' 
 #' @examples
-#' syns <- syno_mycobank(taxon = "Heterobasidion annosum")
+syns <- syno_mycobank(taxon = "Heterobasidion annosum")
 
 syno_mycobank <- function(taxon){
-  require("XML"); require(stringr)
+  require("XML"); require("stringr")
   tax <- strsplit(taxon, "\\s|_")[[1]]
   url <- paste("http://www.mycobank.org/Services/Generic/SearchService.svc/",
-               "rest/xml?layout=14682616000000161&filter=e4060%20CONTAINS%20%22",
-               tax[1], "%20", tax[2], "%22", sep ="")
+    "rest/xml?layout=14682616000000161&filter=e4060%20CONTAINS%20%22",
+    tax[1], "%20", tax[2], "%22", sep ="")
   # parse data from mycobank.org
   par <- xmlTreeParse(url, useInternal = TRUE)
-  syn <- getNodeSet(par, "//e4060")[[1]]
-  syn <- strsplit(as.character(xmlValue(syn)), "=")
-  syn <- syn[[1]][!(lapply(syn[[1]], nchar) == 0)]
+  syn <- getNodeSet(par, "//e4060")[1]
+  syn <- lapply(syn, function(x) strsplit(as.character(xmlValue(x)), "≡|="))
+  syn <- syn[[1]][[1]][!unlist(lapply(syn[[1]], nchar)) == 0]
   # extract species data
   syn <- do.call(rbind, lapply(syn, word, start = 1, end = 4))
   syn <- lapply(syn, function(x) {
-    if(!length(grep("var\\.|spec|sp\\.|ssp\\.|f\\.", x))>0) { x <- word(x, 1, 2) }
+    if(!length(grep("var\\.|spec|sp\\.|ssp\\.|\\bf\\.", x))>0) { x <- word(x, 1, 2) }
     else {x}
   })
   # clean step
-  syn <- gsub("≡", "", syn)
+  syn <- do.call(rbind, syn)
   syn <- c(taxon, syn)
   return(syn)
 }
+
